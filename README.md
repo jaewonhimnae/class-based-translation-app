bc69bced1c819b3b501a5ad8972cca55ef96ff51
John Ahn <smileajw1004@gmail.com>
2020-07-08 13:43:01 +0900

    gitAds Added

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ Thanks for your interest in this repository !
 
 To use this application, 
 
-1. Type  " npm install " inside the root directory  ( Download Front-end Dependencies )
+1. Type  " npm install " in the root directory  ( Download Front-end Dependencies )
 2. Type " npm run start " to run this application
 
 If you have problem, feel free to ask me ^^ 
 
-You can watch the tutorial for this app.
+You can view the tutorial through the link shown below.
 
 https://www.youtube.com/watch?v=cHqxgLhOl5Y
 
--- src/App.js
+++ src/App.js
@@ -1,61 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 
-class App extends Component {
+function App() {
+  const { t } = useTranslation();
 
-  handleClick(lang) {
+  function handleClick(lang) {
     i18next.changeLanguage(lang)
   }
 
-  render() {
-    const { t } = this.props;
-
-    let somthingNeedsTobe = "";
-    let tagetedValue = 1;
-    //when you want it to be rtl 
-    if (tagetedValue === 1) {
-      somthingNeedsTobe = "rtl"
-    } else {
-      somthingNeedsTobe = "ltr"
-    }
-
-    return (
-      <div className="App">
-        <nav style={{ width: '100%', padding: '2rem 0', backgroundColor: 'gray' }}>
-          <button onClick={() => this.handleClick('en')} >
+  return (
+    <div className="App">
+        <nav style={{ width: '100%', padding: '2rem 0', backgroundColor:'gray' }}>
+          <button onClick={()=>handleClick('en')} >
             English
-        </button>
-          <button onClick={() => this.handleClick('ko')} >
-            Korean
-        </button>
-          <button onClick={() => this.handleClick('chi')} >
-            Chinese
           </button>
+          <button onClick={()=>handleClick('ko')} >
+            Korean
+          </button>
+          <button onClick={()=>handleClick('chi')} >
+            Chinese
+         </button>
         </nav>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <h3>{t('Thanks.1')}</h3>  <h3>{t('Why.1')}</h3>
-            {somthingNeedsTobe === rtl ?
-              t('Position1') :
-              t('Position2')
-            }
-            {somthingNeedsTobe === rtl ?
-              //Load External Css A
-              <></>
-              : 
-              <></>
-              //Load External Css B
-            }
-
+            <h3>{t('Thanks.1')}</h3>  <h3>{t('Why.1')}</h3> 
           </p>
         </header>
-      </div>
-    )
-  }
+    </div>
+  );
 }
 
-export default withTranslation()(App);
+export default App;
